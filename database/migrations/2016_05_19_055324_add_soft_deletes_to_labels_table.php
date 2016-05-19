9fbe8e3c9186d89554c5cd28e52292cd7c2d5300
Jaime Zaragoza <hola@jaimeeee.com>
2016-05-19 00:58:48 -0500

    Soft deletes for labels table migration

---
+++ database/migrations/2016_05_19_055324_add_soft_deletes_to_labels_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSoftDeletesToLabelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('labels', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('labels', function (Blueprint $table) {
+            //
+        });
+    }
+}
